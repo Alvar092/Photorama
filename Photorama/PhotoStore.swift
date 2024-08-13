f481351389d8ad0b9c712ce4d48c9c0cff54af0c
Alvar092 <148547498+Alvar092@users.noreply.github.com>
2024-08-13 12:30:57 +0200

    Subo la app acabada a falta de las malditas pantallas negras

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -22,6 +22,11 @@ enum PhotosResult {
     case failure(Error)
 }
 
+enum TagsResult {
+    case success([Tag])
+    case failure(Error)
+}
+
 class PhotoStore {
     
     let imageStore = ImageStore()
@@ -41,11 +46,33 @@ class PhotoStore {
         return URLSession(configuration: config)
     }()
    
-    private func processPhotosRequest(data: Data?, error: Error?) ->  PhotosResult {
+    private func processPhotosRequest(data: Data?, error: Error?, completion: @escaping (PhotosResult)-> Void) {
         guard let jsonData = data else {
-            return .failure(error!)
+            completion(.failure(error!))
+            return
         }
-        return FlickrAPI.photos(fromJSON: jsonData, into: persistentContainer.viewContext)
+        persistentContainer.performBackgroundTask{
+            (context) in
+            
+            let result = FlickrAPI.photos(fromJSON: jsonData, into: context)
+            
+            do{
+                try context.save()
+            } catch {
+                print("Error saving to Core Data: \(error).")
+                completion(.failure(error))
+                return
+            }
+            switch result {
+            case let .success(photos):
+                let photoIDs = photos.map { return $0.objectID }
+                let viewContext = self.persistentContainer.viewContext
+                let viewContextPhotos = photoIDs.map { return viewContext.object(with: $0) } as! [Photo]
+                completion(.success(viewContextPhotos))
+            case .failure:
+                completion(result)
+            }
+        }
     }
     
     func fetchInterestingPhotos(completion: @escaping (PhotosResult) -> Void) {
@@ -55,17 +82,12 @@ class PhotoStore {
         let task = session.dataTask(with: request) {
             (data, response, error) -> Void in
             
-            var result = self.processPhotosRequest(data: data, error: error)
-            if case .success = result {
-                do {
-                    try self.persistentContainer.viewContext.save()
+            self.processPhotosRequest(data: data, error: error) {
+                (result) in
+                
+                OperationQueue.main.addOperation {
+                    completion(result)
                 }
-                catch let error {
-                    result = .failure(error)
-                }
-            }
-            OperationQueue.main.addOperation {
-                completion(result)
             }
         }
         task.resume()
@@ -130,6 +152,22 @@ class PhotoStore {
             do{
                 let allPhotos = try viewContext.fetch(fetchRequest)
                 completion(.success(allPhotos))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func fetchAllTags(completion: @escaping (TagsResult) -> Void) {
+        let fetchRequest: NSFetchRequest<Tag> = Tag.fetchRequest()
+        let sortByName = NSSortDescriptor(key: #keyPath(Tag.name), ascending: true)
+        fetchRequest.sortDescriptors = [sortByName]
+        
+        let viewContext = persistentContainer.viewContext
+        viewContext.perform {
+            do {
+                let allTags = try fetchRequest.execute()
+                completion(.success(allTags))
             } catch {
                 completion(.failure(error))
             }
